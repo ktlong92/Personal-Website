11c643cb2e2db0236e00632db2089331d31c1ecb
Kaleb <kaleb.t.long92@gmail.com>
2023-01-18 21:45:15 -0700

    Set Up Sanity

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'gclg5q4h',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/social.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'experience',
+  title: 'Experience',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      description: 'platform for social media',
+      type: 'string',
+    },
+    {
+      name: 'url',
+      title: 'Url',
+      type: 'url',
+    },
+  ],    
+}
